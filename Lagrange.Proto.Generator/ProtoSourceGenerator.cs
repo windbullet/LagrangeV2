5eabae82953907d31d5ffa9c02e81e101c5e1232
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2025-04-10 06:20:50 +0800

    [Proto] Introduce Source Generators

---
+++ Lagrange.Proto.Generator/ProtoSourceGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Lagrange.Proto.Generator;
+
+public class ProtoSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
