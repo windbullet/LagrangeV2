6987e3ec5aa9aaa8f425f8b86c30404f54a9e9b4
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2025-04-14 02:43:03 +0800

    [Proto] Remove TypeInfo inheritance for ObjectInfo

--- Lagrange.Proto/Serialization/Metadata/ProtoObjectInfo.cs
+++ Lagrange.Proto/Serialization/Metadata/ProtoObjectInfo.cs
@@ -3,7 +3,7 @@
 namespace Lagrange.Proto.Serialization.Metadata;
 
 [DebuggerDisplay("Fields = {Fields.Count}")]
-public class ProtoObjectInfo<T> : ProtoTypeInfo<T>
+public class ProtoObjectInfo<T>
 {
     public Dictionary<int, ProtoFieldInfo> Fields { get; init; } = new();
     
