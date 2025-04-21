395775e67d77e8529e033234a177d3b3c27d4a0b
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2025-04-21 19:57:06 +0800

    [Proto] Add Test for Node

---
+++ Lagrange.Proto.Test/NodeTest.cs
@@ -0,0 +1,85 @@
+using Lagrange.Proto.Nodes;
+using Lagrange.Proto.Serialization;
+using Lagrange.Proto.Utility;
+
+namespace Lagrange.Proto.Test;
+
+[TestFixture]
+public class NodeTest
+{
+    private TestNode _obj;
+    
+    private byte[] _bytes;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _obj = new TestNode
+        {
+            Test1 = 114514
+        };
+
+        _bytes = ProtoSerializer.Serialize(_obj);
+    }
+    
+    [Test]
+    public void TestParse()
+    {
+        var node = ProtoObject.Parse(_bytes);
+        var test = node[1].GetValue<int>();
+        
+        Assert.That(test, Is.EqualTo(_obj.Test1));
+    }
+
+    [Test]
+    public void TestSerialize()
+    {
+        var node = ProtoObject.Parse(_bytes);
+        var bytes = node.Serialize();
+
+        Assert.That(bytes, Is.EqualTo(_bytes));
+    }
+
+    [Test]
+    public void TestConstructor()
+    {
+        var node = new ProtoObject
+        {
+            { 1, new ProtoObject { { 1, 2 } } }, 
+            { 1, new ProtoObject { { 1, 2 } } }, 
+            { 3, 4 },
+            { 5, 6 }
+        };
+
+        var bytes = node.Serialize();
+        Console.WriteLine(Convert.ToHexString(bytes));
+        var parsed = ProtoObject.Parse(bytes);
+
+        int value = parsed[1][0][1].GetValue<int>();
+
+        Assert.That(value, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void TestSerializeToWriter()
+    {
+        var node = new ProtoObject
+        {
+            { 1, new ProtoObject { { 1, 2 } } }, { 1, new ProtoObject { { 1, 2 } } }, { 3, 4 }, { 5, 6 }
+        };
+
+        var bufferWriter = new SegmentBufferWriter(1024);
+        node.Serialize(bufferWriter);
+
+        var bytes = bufferWriter.ToArray();
+        var normalBytes = node.Serialize();
+        
+        Assert.That(bytes, Is.EqualTo(normalBytes));
+    }
+}
+
+[ProtoPackable]
+public partial class TestNode
+{
+    [ProtoMember(1)] public int Test1 { get; set; }
+}
