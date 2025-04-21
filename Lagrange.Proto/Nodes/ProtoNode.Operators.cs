1a6749afda7bf06708155e02e74283df726580ec
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2025-04-21 17:53:02 +0800

    [Proto] Add additional operators

--- Lagrange.Proto/Nodes/ProtoNode.Operators.cs
+++ Lagrange.Proto/Nodes/ProtoNode.Operators.cs
@@ -53,4 +53,30 @@ public partial class ProtoNode
     public static implicit operator ProtoNode(ReadOnlyMemory<char> value) => ProtoValue.Create(value);
     
     public static implicit operator ProtoNode(ReadOnlyMemory<byte> value) => ProtoValue.Create(value);
+    
+    public static explicit operator bool(ProtoNode value) => value.GetValue<bool>();
+    
+    public static explicit operator sbyte(ProtoNode value) => value.GetValue<sbyte>();
+    
+    public static explicit operator byte(ProtoNode value) => value.GetValue<byte>();
+    
+    public static explicit operator short(ProtoNode value) => value.GetValue<short>();
+    
+    public static explicit operator ushort(ProtoNode value) => value.GetValue<ushort>();
+    
+    public static explicit operator int(ProtoNode value) => value.GetValue<int>();
+    
+    public static explicit operator uint(ProtoNode value) => value.GetValue<uint>();
+    
+    public static explicit operator long(ProtoNode value) => value.GetValue<long>();
+    
+    public static explicit operator ulong(ProtoNode value) => value.GetValue<ulong>();
+    
+    public static explicit operator float(ProtoNode value) => value.GetValue<float>();
+    
+    public static explicit operator double(ProtoNode value) => value.GetValue<double>();
+    
+    public static explicit operator string(ProtoNode value) => value.GetValue<string>();
+    
+    public static explicit operator byte[](ProtoNode value) => value.GetValue<byte[]>();
 }
