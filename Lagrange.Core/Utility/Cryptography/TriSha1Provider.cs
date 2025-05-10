915965633acd535f1c592fac9cdd904f201c90da
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2025-05-10 19:32:40 +0800

    [Core] Implemented TriSha1Provider.cs
    Co-Authored-By: 手瓜一十雪 <nanaeonn@outlook.com>
    Co-Authored-By: pk5ls20 <114645197+pk5ls20@users.noreply.github.com>

---
+++ Lagrange.Core/Utility/Cryptography/TriSha1Provider.cs
@@ -0,0 +1,77 @@
+using System.Buffers.Binary;
+
+namespace Lagrange.Core.Utility.Cryptography;
+
+internal static class TriSha1Provider
+{
+    private const int Threshold = 100 * 1024 * 1024;
+    
+    private const int Sha1SampleSize = 30 * 1024 * 1024;
+    
+    private const int SingleSampleSize = 10 * 1024 * 1024;
+    
+    public static void CalculateTriSha1(Stream stream)
+    {
+        if (!stream.CanSeek) throw new ArgumentException("Stream must support seeking/Length.", nameof(stream));
+
+        long length = stream.Length;
+        byte[] sample;
+
+        switch (length)
+        {
+            case > Threshold:
+                sample = GC.AllocateUninitializedArray<byte>(Sha1SampleSize + sizeof(long));
+
+                stream.Position = 0;
+                stream.ReadExactly(sample.AsSpan(0, SingleSampleSize));
+
+                stream.Position = length / 2 - SingleSampleSize / 2;
+                stream.ReadExactly(sample.AsSpan(SingleSampleSize, SingleSampleSize));
+
+                stream.Position = length - SingleSampleSize;
+                stream.ReadExactly(sample.AsSpan(SingleSampleSize * 2, SingleSampleSize));
+                BinaryPrimitives.WriteInt64LittleEndian(sample.AsSpan(Sha1SampleSize), length);
+                break;
+            case <= Sha1SampleSize:
+                sample = GC.AllocateUninitializedArray<byte>((int)length + sizeof(long));
+                stream.Position = 0;
+                stream.ReadExactly(sample.AsSpan(0, (int)length));
+                BinaryPrimitives.WriteInt64LittleEndian(sample.AsSpan((int)length), length);
+                break;
+            default:
+                sample = GC.AllocateUninitializedArray<byte>(Sha1SampleSize + sizeof(long));
+                stream.Position = 0;
+                stream.ReadExactly(sample.AsSpan(0, SingleSampleSize));
+                BinaryPrimitives.WriteInt64LittleEndian(sample.AsSpan(Sha1SampleSize), length);
+                break;
+        }
+
+        stream.Position = 0;
+    }
+    
+    public static void CalculateTriSha1(ReadOnlySpan<byte> data)
+    {
+        byte[] sample;
+        
+        switch (data.Length)
+        {
+            case > Threshold:
+                sample = GC.AllocateUninitializedArray<byte>(Sha1SampleSize + sizeof(long));
+                data.Slice(0, SingleSampleSize).CopyTo(sample);
+                data.Slice(data.Length / 2 - SingleSampleSize / 2, SingleSampleSize).CopyTo(sample.AsSpan(SingleSampleSize));
+                data.Slice(data.Length - SingleSampleSize).CopyTo(sample.AsSpan(SingleSampleSize * 2));
+                BinaryPrimitives.WriteInt64LittleEndian(sample.AsSpan(Sha1SampleSize), data.Length);
+                break;
+            case <= Sha1SampleSize:
+                sample = GC.AllocateUninitializedArray<byte>(data.Length + sizeof(long));
+                data.CopyTo(sample);
+                BinaryPrimitives.WriteInt64LittleEndian(sample.AsSpan(data.Length), data.Length);
+                break;
+            default:
+                sample = GC.AllocateUninitializedArray<byte>(Sha1SampleSize + sizeof(long));
+                data.Slice(0, SingleSampleSize).CopyTo(sample);
+                BinaryPrimitives.WriteInt64LittleEndian(sample.AsSpan(Sha1SampleSize), data.Length);
+                break;
+        }
+    }
+}
