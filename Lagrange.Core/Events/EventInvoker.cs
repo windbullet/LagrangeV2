257fc1dacbc4f2386e9de441f00119c9d4fab99c
DarkRRb <177549718+DarkRRb@users.noreply.github.com>
2025-05-13 02:37:18 +0800

    [Core] Support unregistering event handlers

--- Lagrange.Core/Events/EventInvoker.cs
+++ Lagrange.Core/Events/EventInvoker.cs
@@ -9,27 +9,27 @@ namespace Lagrange.Core.Events;
 public class EventInvoker(BotContext context) : IDisposable
 {
     private const string Tag = nameof(EventInvoker);
-    
+
     private readonly Dictionary<Type, Action<BotContext, EventBase>> _actions = new();
-    
+
     private readonly Dictionary<Type, Func<BotContext, EventBase, Task>> _asyncActions = new();
 
     public delegate void LagrangeEventHandler<in TEvent>(BotContext context, TEvent e) where TEvent : EventBase;
-    
+
     public delegate Task LagrangeAsyncEventHandler<in TEvent>(BotContext context, TEvent e) where TEvent : EventBase;
-    
+
     [UnconditionalSuppressMessage("AOT", "IL3050", Justification = "DynamicMethod is not supported in AOT, but this if block is not executed in AOT")]
     public void RegisterEvent<TEvent>(LagrangeEventHandler<TEvent> handler) where TEvent : EventBase
     {
         Debug.Assert(!handler.Method.IsStatic);
         ArgumentNullException.ThrowIfNull(handler.Target);
-        
+
         if (RuntimeFeature.IsDynamicCodeSupported)
         {
             Type[] args = [handler.Target.GetType(), typeof(BotContext), typeof(EventBase)];
             var method = new DynamicMethod($"EventInvoker_{typeof(TEvent)}", typeof(void), args, typeof(EventInvoker), true);
             var il = method.GetILGenerator();
-            
+
             il.Emit(OpCodes.Ldarg_0); // [Target]
             il.Emit(OpCodes.Ldarg_1); // [Target, BotContext]
             il.Emit(OpCodes.Ldarg_2); // [Target, BotContext, EventBase]
@@ -44,26 +44,26 @@ public class EventInvoker(BotContext context) : IDisposable
             _actions[typeof(TEvent)] = (ctx, e) => handler(ctx, (TEvent)e);
         }
     }
-    
+
     [UnconditionalSuppressMessage("AOT", "IL3050", Justification = "DynamicMethod is not supported in AOT, but this if block is not executed in AOT")]
     public void RegisterEvent<TEvent>(LagrangeAsyncEventHandler<TEvent> handler) where TEvent : EventBase
     {
         Debug.Assert(!handler.Method.IsStatic);
         ArgumentNullException.ThrowIfNull(handler.Target);
-        
+
         if (RuntimeFeature.IsDynamicCodeSupported)
         {
             Type[] args = [handler.Target.GetType(), typeof(BotContext), typeof(EventBase)];
             var method = new DynamicMethod($"EventInvoker_{typeof(TEvent)}", typeof(Task), args, typeof(EventInvoker), true);
             var il = method.GetILGenerator();
-            
+
             il.Emit(OpCodes.Ldarg_0); // [Target]
             il.Emit(OpCodes.Ldarg_1); // [Target, BotContext]
             il.Emit(OpCodes.Ldarg_2); // [Target, BotContext, EventBase]
             il.Emit(OpCodes.Castclass, typeof(TEvent)); // [Target, BotContext, TEvent]
             il.Emit(OpCodes.Callvirt, handler.Method); // [Task]
             il.Emit(OpCodes.Ret); // []
-            
+
             _asyncActions[typeof(TEvent)] = method.CreateDelegate<Func<BotContext, EventBase, Task>>(handler.Target);
         }
         else
@@ -72,10 +72,18 @@ public class EventInvoker(BotContext context) : IDisposable
         }
     }
 
+    public void UnregisterEvent<TEvent>() where TEvent : EventBase
+    {
+        Type type = typeof(TEvent);
+
+        _actions.Remove(type);
+        _asyncActions.Remove(type);
+    }
+
     internal void PostEvent<T>(T @event) where T : EventBase => Task.Run(async () =>
     {
         await context.EventContext.HandleOutgoingEvent(@event);
-        
+
         try
         {
             if (_actions.TryGetValue(typeof(T), out var @delegate))
@@ -89,7 +97,7 @@ public class EventInvoker(BotContext context) : IDisposable
         }
         catch (Exception ex) when (ex is not OperationCanceledException)
         {
-            if (typeof(T) == typeof(BotLogEvent)) 
+            if (typeof(T) == typeof(BotLogEvent))
             {
                 Console.WriteLine($"Failed to post event: {@event}");
                 return;
@@ -98,6 +106,7 @@ public class EventInvoker(BotContext context) : IDisposable
             PostEvent(new BotLogEvent(Tag, LogLevel.Error, $"{ex}"));
         }
     });
+
 
     public void Dispose()
     {
