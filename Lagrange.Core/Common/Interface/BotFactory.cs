4fa95672f422ca079ade330a54be54246b82e639
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2025-05-18 01:46:08 +0800

    [Core] Allow BotAppInfo to be submitted

--- Lagrange.Core/Common/Interface/BotFactory.cs
+++ Lagrange.Core/Common/Interface/BotFactory.cs
@@ -7,15 +7,17 @@ public static class BotFactory
     /// </summary>
     /// <param name="config">The config for Bot</param>
     /// <param name="keystore">Existing Keystore from deserialization</param>
+    /// <param name="appInfo">The app info for Bot, if null, will use default app info from <see cref="BotAppInfo.ProtocolToAppInfo"/></param>
     /// <returns>Created BotContext Instance</returns>
-    public static BotContext Create(BotConfig config, BotKeystore keystore) => 
-        new(config, keystore, BotAppInfo.ProtocolToAppInfo[config.Protocol]);
+    public static BotContext Create(BotConfig config, BotKeystore keystore, BotAppInfo? appInfo = null) =>
+        new(config, keystore, appInfo ?? BotAppInfo.ProtocolToAppInfo[config.Protocol]);
     
     /// <summary>
     /// Create new Bot from Empty
     /// </summary>
     /// <param name="config">The config for Bot</param>
+    /// <param name="appInfo">The app info for Bot, if null, will use default app info from <see cref="BotAppInfo.ProtocolToAppInfo"/></param>
     /// <returns>Created BotContext Instance</returns>
-    public static BotContext Create(BotConfig config) => 
-        new(config, BotKeystore.CreateEmpty(), BotAppInfo.ProtocolToAppInfo[config.Protocol]);
+    public static BotContext Create(BotConfig config, BotAppInfo? appInfo = null) =>
+        new(config, BotKeystore.CreateEmpty(), appInfo ?? BotAppInfo.ProtocolToAppInfo[config.Protocol]);
 }
