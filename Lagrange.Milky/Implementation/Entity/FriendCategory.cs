3eb942ad1e772e2c48e943e28f2a467594fed19a
DarkRRb <177549718+DarkRRb@users.noreply.github.com>
2025-05-21 11:17:43 +0800

    [Milky] Add JsonPropertyName to FriendCategory

--- Lagrange.Milky/Implementation/Entity/FriendCategory.cs
+++ Lagrange.Milky/Implementation/Entity/FriendCategory.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Lagrange.Milky.Implementation.Entity;
 
 public class FriendCategory
 {
+    [JsonPropertyName("category_id")]
     public required int CategoryId { get; init; }
+
+    [JsonPropertyName("category_name")]
     public required string CategoryName { get; init; }
 }
