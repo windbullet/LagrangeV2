938ce74d5aff0b763ced864513635042d57df055
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2025-05-28 21:27:17 +0800

    [Core] Serialization of BotInfo Fixed

--- Lagrange.Core/Common/BotInfo.cs
+++ Lagrange.Core/Common/BotInfo.cs
@@ -3,21 +3,13 @@ using System.Text.Json.Serialization;
 namespace Lagrange.Core.Common;
 
 [Serializable]
-public class BotInfo
+public class BotInfo(byte age, byte gender, string name)
 {
-    [JsonConstructor]
-    internal BotInfo(byte age, byte gender, string name)
-    {
-        Age = age;
-        Gender = gender;
-        Name = name;
-    }
-    
-    public byte Age { get; set; }
-        
-    public byte Gender { get; set; }
-        
-    public string Name { get; set; }
+    public byte Age { get; set; } = age;
+
+    public byte Gender { get; set; } = gender;
+
+    public string Name { get; set; } = name;
 
     public override string ToString() => $"Bot name: {Name} | Gender: {Gender} | Age: {Age}";
 }
