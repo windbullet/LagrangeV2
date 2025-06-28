6334240f0abe20fc547a309f1e3f91d621c4f00f
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2025-06-28 21:04:43 +0800

    [Core] Reworked EventInvoker

--- Lagrange.Core/Events/EventInvoker.cs
+++ Lagrange.Core/Events/EventInvoker.cs
@@ -11,29 +11,42 @@ public sealed class EventInvoker(BotContext context) : IDisposable
 {
     private const string Tag = nameof(EventInvoker);
 
-    private readonly ConcurrentDictionary<Type, Delegate> _syncHandlers = new();
-    private readonly ConcurrentDictionary<Type, Delegate> _asyncHandlers = new();
+    private static readonly ConcurrentDictionary<Delegate, Delegate> SyncHandlerCache = new();
+    private static readonly ConcurrentDictionary<Delegate, Delegate> AsyncHandlerCache = new();
+
+    private readonly ConcurrentDictionary<Type, Delegate?> _syncHandlers = new();
+    private readonly ConcurrentDictionary<Type, Delegate?> _asyncHandlers = new();
 
     public delegate void  LagrangeEventHandler <in TEvent>(BotContext ctx, TEvent e) where TEvent : EventBase;
     public delegate Task  LagrangeAsyncEventHandler<in TEvent>(BotContext ctx, TEvent e) where TEvent : EventBase;
 
     public void RegisterEvent<TEvent>(LagrangeEventHandler<TEvent> handler) where TEvent : EventBase
     {
-        Debug.Assert(!handler.Method.IsStatic);
+        Debug.Assert(handler.Method.IsStatic is false);
         
-        AddHandler(_syncHandlers, typeof(TEvent), BuildSyncDelegate(handler));
+        var wrappedHandler = BuildSyncDelegate(handler);
+        AddHandler(_syncHandlers, typeof(TEvent), wrappedHandler);
     }
 
     public void RegisterEvent<TEvent>(LagrangeAsyncEventHandler<TEvent> handler) where TEvent : EventBase
     {
-        Debug.Assert(!handler.Method.IsStatic);
+        Debug.Assert(handler.Method.IsStatic is false);
         
-        AddHandler(_asyncHandlers, typeof(TEvent), BuildAsyncDelegate(handler));
+        var wrappedHandler = BuildAsyncDelegate(handler);
+        AddHandler(_asyncHandlers, typeof(TEvent), wrappedHandler);
     }
 
-    public void UnregisterEvent<TEvent>(LagrangeEventHandler<TEvent> handler) where TEvent : EventBase => RemoveHandler(_syncHandlers, typeof(TEvent), handler);
+    public void UnregisterEvent<TEvent>(LagrangeEventHandler<TEvent> handler) where TEvent : EventBase
+    {
+        var wrappedHandler = BuildSyncDelegate(handler);
+        RemoveHandler(_syncHandlers, typeof(TEvent), wrappedHandler);
+    }
 
-    public void UnregisterEvent<TEvent>(LagrangeAsyncEventHandler<TEvent> handler) where TEvent : EventBase => RemoveHandler(_asyncHandlers, typeof(TEvent), handler);
+    public void UnregisterEvent<TEvent>(LagrangeAsyncEventHandler<TEvent> handler) where TEvent : EventBase
+    {
+        var wrappedHandler = BuildAsyncDelegate(handler);
+        RemoveHandler(_asyncHandlers, typeof(TEvent), wrappedHandler);
+    }
     
     public void UnregisterEvent<TEvent>() where TEvent : EventBase
     {
@@ -47,12 +60,12 @@ public sealed class EventInvoker(BotContext context) : IDisposable
 
         try
         {
-            if (_syncHandlers.TryGetValue(typeof(T), out var @delegate))
+            if (_syncHandlers.TryGetValue(typeof(T), out var @delegate) && @delegate is not null)
             {
                 ((Action<BotContext, EventBase>)@delegate).Invoke(context, ev);
             }
 
-            if (_asyncHandlers.TryGetValue(typeof(T), out var asyncDelegate))
+            if (_asyncHandlers.TryGetValue(typeof(T), out var asyncDelegate) && asyncDelegate is not null)
             {
                 await ((Func<BotContext, EventBase, Task>)asyncDelegate)(context, ev);
             }
@@ -74,55 +87,66 @@ public sealed class EventInvoker(BotContext context) : IDisposable
         _asyncHandlers.Clear();
     }
 
-    private static void AddHandler(ConcurrentDictionary<Type, Delegate> dict, Type key, Delegate handler)
+    private static void AddHandler(ConcurrentDictionary<Type, Delegate?> dict, Type key, Delegate handler)
     {
         dict.AddOrUpdate(key, handler, (_, old) => Delegate.Combine(old, handler));
     }
 
-    private static void RemoveHandler(ConcurrentDictionary<Type, Delegate> dict, Type key, Delegate handler)
+    private static void RemoveHandler(ConcurrentDictionary<Type, Delegate?> dict, Type key, Delegate handler)
     {
-        dict.AddOrUpdate(key, _ => null!, (_, old) =>
+        dict.AddOrUpdate(key, _ => null, (_, old) => Delegate.Remove(old, handler)!);
+        
+        if (dict.TryGetValue(key, out var currentDelegate) && currentDelegate == null)
         {
-            var updated = Delegate.Remove(old, handler);
-            return updated != null && updated.GetInvocationList().Length > 0 ? updated : null!;
+            dict.TryRemove(key, out _);
+        }
+    }
+
+    [UnconditionalSuppressMessage("AOT", "IL3050", Justification = "DynamicMethod is not supported in AOT, but this branch is skipped there")]
+    private static Delegate BuildSyncDelegate<TEvent>(LagrangeEventHandler<TEvent> h) where TEvent : EventBase
+    {
+        return SyncHandlerCache.GetOrAdd(h, handler =>
+        {
+            if (!RuntimeFeature.IsDynamicCodeSupported)
+            {
+                return (Action<BotContext, EventBase>)((ctx, ev) => ((LagrangeEventHandler<TEvent>)handler)(ctx, (TEvent)ev));
+            }
+
+            var dm = new DynamicMethod($"EventInvoker_{typeof(TEvent).Name}_{Guid.NewGuid()}", typeof(void), [handler.Target!.GetType(), typeof(BotContext), typeof(EventBase)], typeof(EventInvoker), true);
+            var il = dm.GetILGenerator();
+            
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Ldarg_2);
+            il.Emit(OpCodes.Castclass, typeof(TEvent));
+            il.Emit(OpCodes.Callvirt, handler.Method);
+            il.Emit(OpCodes.Ret);
+
+            return dm.CreateDelegate<Action<BotContext, EventBase>>(handler.Target);
         });
-        
-        if (dict.TryRemove(key, out var d) && d is not { }) dict.TryRemove(key, out _); // is not { } is needed to avoid removing the key if the delegate is null, and escape from ReSharper's warning
     }
 
     [UnconditionalSuppressMessage("AOT", "IL3050", Justification = "DynamicMethod is not supported in AOT, but this branch is skipped there")]
-    private static Action<BotContext, EventBase> BuildSyncDelegate<TEvent>(LagrangeEventHandler<TEvent> h) where TEvent : EventBase
+    private static Delegate BuildAsyncDelegate<TEvent>(LagrangeAsyncEventHandler<TEvent> h) where TEvent : EventBase
     {
-        if (!RuntimeFeature.IsDynamicCodeSupported) return (ctx, ev) => h(ctx, (TEvent)ev);
+        return AsyncHandlerCache.GetOrAdd(h, handler =>
+        {
+            if (!RuntimeFeature.IsDynamicCodeSupported)
+            {
+                return (Func<BotContext, EventBase, Task>)((ctx, ev) => ((LagrangeAsyncEventHandler<TEvent>)handler)(ctx, (TEvent)ev));
+            }
 
-        var dm = new DynamicMethod($"EventInvoker_{typeof(TEvent).Name}", typeof(void), [h.Target!.GetType(), typeof(BotContext), typeof(EventBase)], typeof(EventInvoker), true);
-        var il = dm.GetILGenerator();
-        
-        il.Emit(OpCodes.Ldarg_0);
-        il.Emit(OpCodes.Ldarg_1);
-        il.Emit(OpCodes.Ldarg_2);
-        il.Emit(OpCodes.Castclass, typeof(TEvent));
-        il.Emit(OpCodes.Callvirt, h.Method);
-        il.Emit(OpCodes.Ret);
+            var dm = new DynamicMethod($"EventInvoker_{typeof(TEvent).Name}_Async_{Guid.NewGuid()}", typeof(Task), [handler.Target!.GetType(), typeof(BotContext), typeof(EventBase)], typeof(EventInvoker), true);
+            var il = dm.GetILGenerator();
+            
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Ldarg_2);
+            il.Emit(OpCodes.Castclass, typeof(TEvent));
+            il.Emit(OpCodes.Callvirt, handler.Method);
+            il.Emit(OpCodes.Ret);
 
-        return dm.CreateDelegate<Action<BotContext, EventBase>>(h.Target);
-    }
-
-    [UnconditionalSuppressMessage("AOT", "IL3050", Justification = "DynamicMethod is not supported in AOT, but this branch is skipped there")]
-    private static Func<BotContext, EventBase, Task> BuildAsyncDelegate<TEvent>(LagrangeAsyncEventHandler<TEvent> h) where TEvent : EventBase
-    {
-        if (!RuntimeFeature.IsDynamicCodeSupported) return (ctx, ev) => h(ctx, (TEvent)ev);
-
-        var dm = new DynamicMethod($"EventInvoker_{typeof(TEvent).Name}_Async", typeof(Task), [h.Target!.GetType(), typeof(BotContext), typeof(EventBase)], typeof(EventInvoker), true);
-        var il = dm.GetILGenerator();
-        
-        il.Emit(OpCodes.Ldarg_0);
-        il.Emit(OpCodes.Ldarg_1);
-        il.Emit(OpCodes.Ldarg_2);
-        il.Emit(OpCodes.Castclass, typeof(TEvent));
-        il.Emit(OpCodes.Callvirt, h.Method);
-        il.Emit(OpCodes.Ret);
-
-        return dm.CreateDelegate<Func<BotContext, EventBase, Task>>(h.Target);
+            return dm.CreateDelegate<Func<BotContext, EventBase, Task>>(handler.Target);
+        });
     }
 }
